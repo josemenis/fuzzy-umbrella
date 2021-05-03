de9134a23280ffac657b263b418b74bcf0bf29f6
Jose M <joseamenis@gmail.com>
2021-05-02 22:14:43 -0700

    Used AVD manager in android studio to run program

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.happybirthday
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.os.Bundlegit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
